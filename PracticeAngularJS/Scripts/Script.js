a569c2192f4dae4f633900f9fdb9a86d7a8d95b3
Yang09701194 <mustnecessity@gmail.com>
2018-06-14 17:43:51 +0800

    Lesson 2 done!

---
+++ PracticeAngularJS/Scripts/Script.js
@@ -0,0 +1,12 @@
+/// <reference path="angular.min.js" />
+
+var myApp = angular.module("myModule", [])
+
+var myController = function($scope){
+    $scope.message = "AngularJS Tutorial";
+}
+
+myApp.controller("myController", myController);
+
+
+
