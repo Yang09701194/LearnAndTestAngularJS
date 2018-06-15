6f1c65b3a4e0b753c757066c9315c2a1e710e95d
Yang09701194 <mustnecessity@gmail.com>
2018-06-15 11:51:15 +0800

    lesson 4 pass!!

--- PracticeAngularJS/Scripts/Script.js
+++ PracticeAngularJS/Scripts/Script.js
@@ -2,20 +2,37 @@
 
 var myApp = angular.module("myModule", [])
 
-var myController = function($scope){
+var myController = function ($scope) {
     $scope.message = "AngularJS Tutorial";
 
     var employee = {
-        firstName : "David",
-        lastName : "Hastings",
-        gender : "Male"
+        firstName: "David",
+        lastName: "Hastings",
+        gender: "Male"
     };
 
-    $scope.employee = employee;
+    var country = {
+        name: "Taiwan",
+        capital: "Taipei",
+        flag: "Images/advice-vaccines-taiwan.jpg"
+    }
 
-}   
+    $scope.employee = employee;
+    $scope.country = country;
+
+}
 
 myApp.controller("myController", myController);
 
+//可以直接把duncrion  放在 Controller 第二個參數   
+//myApp.controller("myController",  function($scope){
+//   $scope.message = "AngularJS Tutorial";)  ..... 
+//}
 
+//也可以用類似  nhibernate 的  程式碼的config方式   用  function串聯的方式  或是連珠砲?
+//或者是LINQ的連續查詢
+//angular.module("myModule", [])
+//.controller("myController",  function($scope){
+//   $scope.message = "AngularJS Tutorial";)  ..... 
+//})
 
