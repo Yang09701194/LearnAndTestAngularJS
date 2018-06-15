94a84c9b68d6b06a8bfcfd777abe145f344ec749
Yang09701194 <mustnecessity@gmail.com>
2018-06-15 10:19:58 +0800

    lesson 3 done!

--- PracticeAngularJS/Scripts/Script.js
+++ PracticeAngularJS/Scripts/Script.js
@@ -4,7 +4,16 @@ var myApp = angular.module("myModule", [])
 
 var myController = function($scope){
     $scope.message = "AngularJS Tutorial";
-}
+
+    var employee = {
+        firstName : "David",
+        lastName : "Hastings",
+        gender : "Male"
+    };
+
+    $scope.employee = employee;
+
+}   
 
 myApp.controller("myController", myController);
 
